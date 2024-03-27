f0b5240f216e8afe69d6ff50cd91c8356443c1e6
AyeshaShahzad297 <ayeshashahzad996@gmail.com>
2024-03-27 15:58:07 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1); // +1 to generate a number from 1 to 6.
+const guessedNumber = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please enter a number between 1-6: ",
+    },
+]);
+if (guessedNumber.userGuessedNumber === randomNumber) {
+    console.log("Good Job! You guessed a right number.");
+}
+else {
+    console.log("You guessed a wrong number.");
+}
